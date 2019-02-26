3a5ee7b7849e6fbeba24e2a4b185ba126ddec793
Joshua Cearley <joshua.cearley@gmail.com>
2019-02-25 21:17:52 -0600

    re-enable adding the recorder module

--- src/skylights.cc
+++ src/skylights.cc
@@ -9,7 +9,7 @@ void init(Plugin *p) {
 	p->version = TOSTRING(VERSION);
 
 	// Add all Models defined throughout the plugin
-	//p->addModel(recorder_model);
+	p->addModel(recorder_model);
 	p->addModel(whatnote_model);
 	p->addModel(turing_model);
 	p->addModel(turing_volts_model);
