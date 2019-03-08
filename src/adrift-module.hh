ccc434bda3d06b29a922f0561e6b632c7c7ecb84
Joshua Cearley <joshua.cearley@gmail.com>
2019-03-07 19:19:47 -0600

    add five more channels to the adrift

--- src/adrift-module.hh
+++ src/adrift-module.hh
@@ -3,17 +3,18 @@
 #include <dsp/digital.hpp>
 
 struct adrift_module: public Module {
-   static const size_t channels = 1;
+   static const size_t channels = 6;
 
    enum ParamIds {
       P_ATTENUATOR,
-      P_BIP0,
+      P_BIP0, P_BIP1, P_BIP2, P_BIP3, P_BIP4, P_BIP5,
       NUM_PARAMS
    };
+
    enum InputIds {
       I_TRIG_ALL,
-      I_TRIG0,
-      I_CV0,
+      I_TRIG0, I_TRIG1, I_TRIG2, I_TRIG3, I_TRIG4, I_TRIG5,
+      I_CV0, I_CV1, I_CV2, I_CV3, I_CV4, I_CV5,
       NUM_INPUTS
    };
    enum OutputIds {
