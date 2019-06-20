41445f106a8247426fb9a4c35848e38ab19b9963
Kent Williams <kwilliams@leepfrog.com>
2019-06-20 14:53:08 -0500

    Fixed ports failing to instantiate (thanks to BaconPaul

--- src/components.hh
+++ src/components.hh
@@ -3,6 +3,7 @@
 
 struct DavidLTPort : public SvgPort {
    DavidLTPort() {
-      setSvg(APP->window->loadSvg("res/cntr_LT.svg"));
+		setSvg(APP->window->loadSvg(asset::plugin(pluginInstance,"res/cntr_LT.svg")));
+
    }
 };
