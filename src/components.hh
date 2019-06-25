fc06a377d88e1281b40d11a1edf49a52a5f388b7
Joshua Cearley <joshua.cearley@gmail.com>
2019-06-24 22:55:22 -0500

    fix setSVG deprecation
    this is a stupid/pointless deprecation, but whatever

--- src/components.hh
+++ src/components.hh
@@ -2,7 +2,7 @@
 #pragma once
 
 struct DavidLTPort : public SVGPort {
-   DavidLTPort() {	
-      setSVG(APP->window->loadSvg(asset::plugin(pluginInstance, "res/cntr_LT.svg")));
+   DavidLTPort() {
+      setSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/cntr_LT.svg")));
    }
 };
