b1c4f12cae432ac87717c1d44ae5f7ca98240937
Joshua Cearley <joshua.cearley@gmail.com>
2019-07-21 02:41:44 -0500

    Ran python scripts through Black.

--- src/modules.py
+++ src/modules.py
@@ -1,47 +1,46 @@
-
 from tags import ModuleTag
 
 simple_plugins = [
-   {
-      'name': 'adrift',
-      'plugin': 'Skylights',
-      'slug': 'SkAdrift',
-      'friendly_name': 'Adrift (Analogue Drift)',
-      'tags': [ModuleTag.SAMPLE_AND_HOLD_TAG],
-   },
-   {
-      'name': 'turing',
-      'plugin': "Skylights",
-      'slug': "SkTuringV2",
-      'friendly_name': "Alan (Turing Machine)",
-      'tags': [ModuleTag.SEQUENCER_TAG],
-   },
-   {
-      'name': 'turing_pulse',
-      'plugin': "Skylights",
-      'slug': "SkTuringPulse",
-      'friendly_name': "Alan (Pulse Expander)",
-      'tags': [ModuleTag.UTILITY_TAG],
-   },
-   {
-      'name': 'turing_volts',
-      'plugin': "Skylights",
-      'slug': "SkTuringVolts",
-      'friendly_name': "Alan (Volts Expander)",
-      'tags': [ModuleTag.UTILITY_TAG],
-   },
-   {
-      'name': 'turing_digital',
-      'plugin': 'Skylights',
-      'slug': 'SkTuringVactrol',
-      'friendly_name': "Alan (Digital Mix Expander)",
-      'tags': [ModuleTag.UTILITY_TAG],
-   },
-   {
-      'name': 'turing_vactrol',
-      'plugin': 'Skylights',
-      'slug': 'SkTuringVactrolAnalogue',
-      'friendly_name': "Alan (Vactrol Mix Expander)",
-      'tags': [ModuleTag.UTILITY_TAG],
-   },
+    {
+        "name": "adrift",
+        "plugin": "Skylights",
+        "slug": "SkAdrift",
+        "friendly_name": "Adrift (Analogue Drift)",
+        "tags": [ModuleTag.SAMPLE_AND_HOLD_TAG],
+    },
+    {
+        "name": "turing",
+        "plugin": "Skylights",
+        "slug": "SkTuringV2",
+        "friendly_name": "Alan (Turing Machine)",
+        "tags": [ModuleTag.SEQUENCER_TAG],
+    },
+    {
+        "name": "turing_pulse",
+        "plugin": "Skylights",
+        "slug": "SkTuringPulse",
+        "friendly_name": "Alan (Pulse Expander)",
+        "tags": [ModuleTag.UTILITY_TAG],
+    },
+    {
+        "name": "turing_volts",
+        "plugin": "Skylights",
+        "slug": "SkTuringVolts",
+        "friendly_name": "Alan (Volts Expander)",
+        "tags": [ModuleTag.UTILITY_TAG],
+    },
+    {
+        "name": "turing_digital",
+        "plugin": "Skylights",
+        "slug": "SkTuringVactrol",
+        "friendly_name": "Alan (Digital Mix Expander)",
+        "tags": [ModuleTag.UTILITY_TAG],
+    },
+    {
+        "name": "turing_vactrol",
+        "plugin": "Skylights",
+        "slug": "SkTuringVactrolAnalogue",
+        "friendly_name": "Alan (Vactrol Mix Expander)",
+        "tags": [ModuleTag.UTILITY_TAG],
+    },
 ]
--- src/test_filenames.py
+++ src/test_filenames.py
@@ -1,21 +1,19 @@
-
 import unittest
 import glob
 import re
 
-header_regex = re.compile('([a-z0-9]+-)*([a-z0-9]+).hh')
-source_regex = re.compile('([a-z0-9]+-)*([a-z0-9]+).cc')
+header_regex = re.compile("([a-z0-9]+-)*([a-z0-9]+).hh")
+source_regex = re.compile("([a-z0-9]+-)*([a-z0-9]+).cc")
 
 class TestFilenameConvention(unittest.TestCase):
-   def test_headers(self):
-      headers = glob.glob('*.hh')
-   
-      for f in headers:
-         self.assertTrue(header_regex.match(f), f)
-      
-   def test_sources(self):
-      sources = glob.glob('*.cc')
-   
-      for f in sources:
-         self.assertTrue(source_regex.match(f), f)
-   
+    def test_headers(self):
+        headers = glob.glob("*.hh")
+
+        for f in headers:
+            self.assertTrue(header_regex.match(f), f)
+
+    def test_sources(self):
+        sources = glob.glob("*.cc")
+
+        for f in sources:
+            self.assertTrue(source_regex.match(f), f)
